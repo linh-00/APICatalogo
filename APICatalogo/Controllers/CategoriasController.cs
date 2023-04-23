e21c759924dc5501e81253d1c1ca8a32be3e097b
linh-00 <alinnelauren@gmail.com>
2023-04-23 19:07:57 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,103 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Models.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            try
+            {
+                throw new DataMisalignedException();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um problema ao retornar sua solicitação");
+            }
+        }
+
+        [HttpGet("{id:int}", Name ="ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            try
+            {
+
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if(categoria == null)
+            {
+                return NotFound($"Categoria com id= {id} não encontrada");
+            }
+        return Ok(categoria);
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ocorreu um problema ao retornar sua solicitação");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria) 
+        {
+            if (categoria is null)
+                return BadRequest("Dados Inválidos");
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria",
+                new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+
+        public ActionResult Put(int id, Categoria categoria)
+        {
+           if (id != categoria.CategoriaId)
+            {
+                return BadRequest("Dados Inválidos");
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound($"Categoria de id= {id} não encontrada...");
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+    }
+}
---
+++ APICatalogo/Models/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Models.Context
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
